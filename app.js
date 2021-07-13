995b21822cef59ef17139080fef4d5dc53e46565
jk <jagadhishkmr@gmail.com>
2021-07-13 16:02:29 +0530

    json

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 // const bodyPraser = require('body-praser');
 // const path = require('path');
 const bodyParser = require('body-parser');
-const userRoutes = require('./router');
+// const userRoutes = require('./router');
+const DATA = require('./data');
 const path = require('path');
 const port = process.env.PORT || 5000;
 
@@ -15,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
-app.use('/',userRoutes);
-
+app.get('/student/studentLists',(req,res)=>{
+        res.send(DATA);});
 app.listen(PORT, () => {
     console.log(`server started http://localhost:${PORT}`);
 })
